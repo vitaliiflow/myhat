1c874da294c873ccd0939e3355db2be49bca6123
Vitalii Omelchenko <67092563+vitaliiomelchenko@users.noreply.github.com>
2024-02-14 16:46:33 +0200

    Created front-page blocks structure in the theme

--- dist/main.js
+++ dist/main.js
@@ -1,3 +1,23 @@
 "use strict";
 
 console.log("Hello world!");
+"use strict";
+
+jQuery(document).ready(function ($) {
+  $(".slider-full__list").slick({
+    speed: 1000,
+    infinite: true,
+    arrows: false,
+    // autoplay: true,
+    // autoplaySpeed: 7500,
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [{
+      breakpoint: 768,
+      settings: {
+        adaptiveHeight: true
+      }
+    }]
+  });
+});
