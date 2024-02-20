8a132d49698e8db0e39e29c0f44b282463b73fd7
Vitalii Omelchenko <67092563+vitaliiomelchenko@users.noreply.github.com>
2024-02-20 10:12:36 +0200

    Footer 3/3

--- template-parts/footer/footer.php
+++ template-parts/footer/footer.php
@@ -60,6 +60,62 @@ $rightColContent = get_field('right_tab_content','option');
 
         <div class="col-sm col-lg-4 footer__nav">
 
+        <?php if ($footerNav) : ?>
+
+          <div class="accordion footer__accordion mb-5">
+
+            <div class="accordion__list">
+
+              <?php $i = 1; ?>
+
+              <?php foreach($footerNav as $item): ?>
+
+                <?php
+                $label = $item['accordion_label'];
+                $childElements = $item['child_elements'];
+                ?>
+
+                <div class="accordion__item mb-3 js-accordion-item">
+                  <button class="accordion__button js-accordion-button h6" type="button" data-for="<?php echo $i ?>">
+                    <?php echo $label ?>
+                    <div class="accordion__button-icon js-accordion-button-icon"><?php echo get_inline_svg('down-chevron-svgrepo-com.svg');?></div>
+                  </button>
+                  <div class="accordion__body js-accordion-body" data-body="<?php echo $i ?>">
+                    <div class="accordion__flex">
+                      <ul class="accordion__content">
+
+                        <?php foreach($childElements as $element): 
+                          
+                          $link = $element['link'];
+                          if( $link ): 
+                            $link_url = $link['url'];
+                            $link_title = $link['title'];
+                            $link_target = $link['target'] ? $link['target'] : '_self';
+                            ?>
+
+                            <li class="mb-1">
+                              <a class="button" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>
+                            </li>
+                            
+                        <?php endif; ?>
+
+                        <?php endforeach; ?>
+
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+
+                <?php $i++ ?>
+
+              <?php endforeach; ?>
+
+            </div>
+
+          </div>
+
+        <?php endif; ?>
+
         <?php if ($footerNavfooter) : ?>
 
           <div class="footer__nav-footer">
