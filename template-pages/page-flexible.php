a261b5c5b809f47426d394a9a448e1968fb17a89
Vitalii Omelchenko <67092563+vitaliiomelchenko@users.noreply.github.com>
2024-02-24 10:15:44 +0200

    Align breadcrumbs from flexible page with regular page

--- template-pages/page-flexible.php
+++ template-pages/page-flexible.php
@@ -4,7 +4,19 @@
  * Template name: Flexible template
  */
 
-get_header(); ?>
+get_header();
+
+?>
+
+<?php if (!is_front_page()) : ?>
+
+    <div class="breadcrumbs__wrapper pt-3">
+        <div class="container">
+            <?php get_template_part('template-parts/parts/breadcrumbs'); ?>
+        </div>
+    </div>
+
+<?php endif; ?>
 
 
 <?php the_acf_loop();?>
