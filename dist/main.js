e7eb4df1449a5fa9162ee6ffa061762247e4852f
HlibPanchenko <glebpanchenk7@gmail.com>
2024-02-27 12:23:24 +0200

    search on pages teams-archive and varumarken-page

--- dist/main.js
+++ dist/main.js
@@ -324,6 +324,67 @@ jQuery(document).ready(function ($) {
 });
 "use strict";
 
+jQuery(document).ready(function ($) {
+  $('.singleProduct__gallery').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: true
+  });
+  $('.singleProduct__sizeTitle').click(function () {
+    $(this).parent().toggleClass('opened');
+    $(this).parent().find('.singleProduct__sizeList').slideToggle();
+  });
+  $('.singleProduct__accordionItem__title').click(function () {
+    $(this).toggleClass('opened');
+    $(this).parent().find('.singleProduct__accordionItem__content').slideToggle();
+  });
+  $('.quantity-btn').click(function () {
+    var item = $(this).parent().find('.qty'),
+      number = parseInt(item.val());
+    if ($(this).hasClass('increase')) {
+      item.val(parseInt(item.val()) + 1);
+      if (parseInt(item.val()) == $(this).attr('max')) {
+        $('.increase').addClass('disabled');
+      }
+    }
+    if ($(this).hasClass('decrease')) {
+      item.val(parseInt(item.val()) - 1);
+      if (parseInt(item.val()) <= 1) {
+        $('.decrease').addClass('disabled');
+      }
+    }
+    if (parseInt(item.val()) > 1 && parseInt(item.val()) != $(this).attr('max')) {
+      $('.quantity-btn').removeClass('disabled');
+    }
+  });
+  $('.attributes-picker-item').click(function () {
+    var element = $(this),
+      elementAttr = element.attr('data-attribute'),
+      attr = element.closest('.attributes-picker-list').attr('data-attribute-name');
+    $('.attributes-picker-item').removeClass('active');
+    element.addClass('active');
+    $("#".concat(attr)).prop('selectedIndex', $("#".concat(attr, " option[value=\"").concat(elementAttr, "\"]")).index());
+    $("#".concat(attr, " option[value=\"").concat(elementAttr, "\"]")).change();
+    if (attr == 'pa_storlek') {
+      $('.singleProduct__sizeWrapper').find('.singleProduct__sizeTitle').html(element.html());
+    }
+  });
+  $('.variations_form select').each(function () {
+    $(this).find('option').each(function () {
+      if (typeof $(this).attr('selected') !== 'undefined' && $(this).attr('selected') !== false) {
+        console.log(123);
+        var listitem = $("div[data-attribute-name=\"".concat($(this).parent().attr('id'), "\"] div[data-attribute=\"").concat($(this).attr('value'), "\"]"));
+        listitem.addClass('active');
+        if ($(this).parent().attr('name') == 'attribute_pa_storlek') {
+          $('.singleProduct__sizeWrapper').find('.singleProduct__sizeTitle').html(listitem.html());
+        }
+      }
+    });
+  });
+});
+"use strict";
+
 function _typeof(obj) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (obj) { return typeof obj; } : function (obj) { return obj && "function" == typeof Symbol && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }, _typeof(obj); }
 function _defineProperty(obj, key, value) { key = _toPropertyKey(key); if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 function _toPropertyKey(arg) { var key = _toPrimitive(arg, "string"); return _typeof(key) === "symbol" ? key : String(key); }
@@ -474,65 +535,4 @@ jQuery(document).ready(function ($) {
       });
     });
   }
-});
-"use strict";
-
-jQuery(document).ready(function ($) {
-  $('.singleProduct__gallery').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    dots: true
-  });
-  $('.singleProduct__sizeTitle').click(function () {
-    $(this).parent().toggleClass('opened');
-    $(this).parent().find('.singleProduct__sizeList').slideToggle();
-  });
-  $('.singleProduct__accordionItem__title').click(function () {
-    $(this).toggleClass('opened');
-    $(this).parent().find('.singleProduct__accordionItem__content').slideToggle();
-  });
-  $('.quantity-btn').click(function () {
-    var item = $(this).parent().find('.qty'),
-      number = parseInt(item.val());
-    if ($(this).hasClass('increase')) {
-      item.val(parseInt(item.val()) + 1);
-      if (parseInt(item.val()) == $(this).attr('max')) {
-        $('.increase').addClass('disabled');
-      }
-    }
-    if ($(this).hasClass('decrease')) {
-      item.val(parseInt(item.val()) - 1);
-      if (parseInt(item.val()) <= 1) {
-        $('.decrease').addClass('disabled');
-      }
-    }
-    if (parseInt(item.val()) > 1 && parseInt(item.val()) != $(this).attr('max')) {
-      $('.quantity-btn').removeClass('disabled');
-    }
-  });
-  $('.attributes-picker-item').click(function () {
-    var element = $(this),
-      elementAttr = element.attr('data-attribute'),
-      attr = element.closest('.attributes-picker-list').attr('data-attribute-name');
-    $('.attributes-picker-item').removeClass('active');
-    element.addClass('active');
-    $("#".concat(attr)).prop('selectedIndex', $("#".concat(attr, " option[value=\"").concat(elementAttr, "\"]")).index());
-    $("#".concat(attr, " option[value=\"").concat(elementAttr, "\"]")).change();
-    if (attr == 'pa_storlek') {
-      $('.singleProduct__sizeWrapper').find('.singleProduct__sizeTitle').html(element.html());
-    }
-  });
-  $('.variations_form select').each(function () {
-    $(this).find('option').each(function () {
-      if (typeof $(this).attr('selected') !== 'undefined' && $(this).attr('selected') !== false) {
-        console.log(123);
-        var listitem = $("div[data-attribute-name=\"".concat($(this).parent().attr('id'), "\"] div[data-attribute=\"").concat($(this).attr('value'), "\"]"));
-        listitem.addClass('active');
-        if ($(this).parent().attr('name') == 'attribute_pa_storlek') {
-          $('.singleProduct__sizeWrapper').find('.singleProduct__sizeTitle').html(listitem.html());
-        }
-      }
-    });
-  });
 });
