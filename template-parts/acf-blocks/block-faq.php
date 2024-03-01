f4051c4dad17d044c07d2e45ccadaa546214de0f
Vitalii Omelchenko <67092563+vitaliiomelchenko@users.noreply.github.com>
2024-03-01 09:07:33 +0200

    Block: FAQ opened development

--- template-parts/acf-blocks/block-faq.php
+++ template-parts/acf-blocks/block-faq.php
@@ -1,6 +1,6 @@
 <?php 
 
-$tabs_title = get_field('tabs_title');
+$tabs_title = get_sub_field('tabs_title');
 
 ?>
 
@@ -9,39 +9,37 @@ $tabs_title = get_field('tabs_title');
     <section class="section faq-opened bg-color bg-color--white">
         <div class="container">
 
+                <?php if($tabs_title): ?>
 
-                <div class="contact__accordionList__wrapper">
-                    <?php if($tabs_title): ?>
-                        <h3 class="contact__accordionList__title contact-title font-family-primary">
-                            <?php echo $tabs_title; ?>
-                        </h3>
-                    <?php endif; ?>
-                    <?php if(have_rows('tabs')): ?>
-                        <div class="contact__accordionList">
-                            <?php while(have_rows('tabs')): the_row(); ?>
-                                <?php 
-                                $title = get_sub_field('tab_title');
-                                $content = get_sub_field('tab_content');
-                                if($title || $content):
-                                ?>
-                                    
-                                        <?php if($title): ?>
-                                            <h4 class="contact__accordionList__itemTitle">
-                                                <span class="contact__accordionList__itemTitle__icon">
-                                                    <?php echo get_inline_svg('accordion__arrow.svg'); ?>
-                                                </span> 
-                                                <?php echo $title; ?>
-                                            </h4>
-                                        <?php endif; ?>
-                                        <?php if($content): ?>
-                                            <div class="contact__accordionList__itemContent"><?php echo $content; ?></div>
-                                        <?php endif; ?>
-                                    
-                                <?php endif; ?>
-                            <?php endwhile; ?>
-                        </div>
-                    <?php endif; ?>
-                </div>
+                    <div class="section__title">
+                        <h2><?php echo $tabs_title; ?></h2>
+                    </div>
+
+                <?php endif; ?>
+
+
+
+                <ul class="faq-opened__list row justify-content-lg-between">
+
+                    <?php while(have_rows('tabs')): the_row(); 
+
+                        $title = get_sub_field('tab_title');
+                        $content = get_sub_field('tab_content');
+
+                        if($title && $content): ?>
+
+                            <li class="col-md-6 col-lg-5 faq-opened__item">
+
+                                <h4 class="mb-3"><?php echo $title; ?></h4>
+                                <p><?php echo $content; ?></p>
+
+                            </li>
+                            
+                        <?php endif; ?>
+
+                    <?php endwhile; ?>
+                </ul>
+
 
         </div>
     </section>
