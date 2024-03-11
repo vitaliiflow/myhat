0726f6df9c0bbdf34f66e12627d108007ec73791
Vitalii Omelchenko <67092563+vitaliiomelchenko@users.noreply.github.com>
2024-03-11 11:14:31 +0200

    Fix brands reset search results layout

--- dist/main.js
+++ dist/main.js
@@ -592,16 +592,16 @@ jQuery(document).ready(function ($) {
         brandsList.empty();
         if (response.success && response.data.length > 0) {
           response.data.forEach(function (brand) {
-            var brandItem = $('<li>').addClass('col-4 col-sm-3 col-md-2 brands-page__item');
-            var brandLink = $('<a>').attr('href', brand.url).addClass('brands-page__item-link');
             if (brand.logo) {
+              var brandItem = $('<li>').addClass('col-4 col-sm-3 col-md-2 brands-page__item');
+              var brandLink = $('<a>').attr('href', brand.url).addClass('brands-page__item-link');
               var imgWrapper = $('<div>').addClass('img-wrapper d-flex');
               var image = $('<img>').attr('src', brand.logo).attr('alt', brand.name);
               imgWrapper.append(image);
               brandLink.append(imgWrapper);
+              brandItem.append(brandLink);
+              brandsList.append(brandItem);
             }
-            brandItem.append(brandLink);
-            brandsList.append(brandItem);
           });
         } else {
           brandsList.html('<div>' + codelibry.strings.noBrandsFound + '</div>');
