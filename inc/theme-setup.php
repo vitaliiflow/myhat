e404a75b881fa6ec02f5b440475c514511770223
Vitalii Omelchenko <67092563+vitaliiomelchenko@users.noreply.github.com>
2024-03-26 15:29:05 +0200

    add image sizes and icons for teams to menu

--- inc/theme-setup.php
+++ inc/theme-setup.php
@@ -27,8 +27,8 @@ function codelibry_setup(){
       'languages-menu' => __('Languages Menu', 'theme-name'),
     )
   );
-  add_image_size( 'home-category-icons', 180 ); 
-	add_image_size( 'home-banner', 850, 700, true );
+  add_image_size( 'home-category-icons', 180, 180, true ); // 300 pixels wide (and unlimited height)
+	add_image_size( 'home-banner', 850, 700, true ); // 300 pixels wide (and unlimited height)
 }
 
 add_action( 'after_setup_theme', 'codelibry_setup', 0 );
@@ -40,7 +40,7 @@ add_filter('wp_nav_menu_objects', 'my_wp_nav_menu_objects', 10, 2);
 function my_wp_nav_menu_objects( $items, $args ) {
     
     // loop
-    foreach( $items as &$item ) {
+    foreach( $items as $item ) {
         
         // vars
         $icon = get_field('menu_item_icon_checkbox', $item);
@@ -48,14 +48,24 @@ function my_wp_nav_menu_objects( $items, $args ) {
         // append icon
         if( $icon ) {
             $image_id = get_term_meta( $item->object_id, 'thumbnail_id', true );
-            $post_thumbnail_img = wp_get_attachment_image_src( $image_id, 'home-category-icons' );
-
-            $item->classes[] = 'has-mobile-icon'; // No need for `.=` here
+            $post_thumbnail_img = wp_get_attachment_image_src( $image_id, 'full' );
+            
+            $item->classes[] = 'has-mobile-icon ' . ' ' . $item->object_id;; // No need for `.=` here
             if (is_array($post_thumbnail_img)) { // Check if $post_thumbnail_img is an array
-                $item->title .= '<img fetchPriority="low" src="' . $post_thumbnail_img[0] . '"/>';
+                $item->title .= '<img src="' . $post_thumbnail_img[0] . '"/>';
                 
+            } else  {
+              $term = get_term($item->object_id);
+              $team_thumbnail = get_field('taxonomy-image', $term );
+
+              //var_dump($team_thumbnail)
+
+              if (!empty($team_thumbnail)) {
+                $item->title .= '<img src="' . $team_thumbnail['url'] . '"/>';
+              }
+              
             }
-        }
+      }
     }
     
     // return
